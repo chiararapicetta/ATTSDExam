1b0b790f1f2a1365b0e0a5bf9539368ee1ee0abb
ChiaraRapicetta <chiara.rapicetta@stud.unifi.it>
2019-03-29 10:20:27 +0100

    Auto generate methods equals, hashCode

--- spring-project/src/main/java/attsd/exam/spring/project/model/Restaurant.java
+++ spring-project/src/main/java/attsd/exam/spring/project/model/Restaurant.java
@@ -26,7 +26,38 @@ public class Restaurant {
 	
 	@Override
 	public String toString() {
-		return "Restaurant [id = " + id + ", name = " + name + ", average price = " + averagePrice + "]";
+		return "Restaurant [id=" + id + ", name=" + name + ", averagePrice=" + averagePrice + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + averagePrice;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurant other = (Restaurant) obj;
+		if (averagePrice != other.averagePrice)
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 	
 
