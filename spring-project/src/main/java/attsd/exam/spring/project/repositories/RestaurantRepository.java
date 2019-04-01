fb4785337c7385aef872f4520582c92ef88d24b6
ChiaraRapicetta <chiara.rapicetta@stud.unifi.it>
2019-04-01 15:16:27 +0200

    Modified classes

--- spring-project/src/main/java/attsd/exam/spring/project/repositories/RestaurantRepository.java
+++ spring-project/src/main/java/attsd/exam/spring/project/repositories/RestaurantRepository.java
@@ -2,23 +2,16 @@ package attsd.exam.spring.project.repositories;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.datastax.driver.core.Session;
 
 import attsd.exam.spring.project.model.Restaurant;
 
 @Repository
 public class RestaurantRepository {
 
-	private static final String TABLE_NAME = "RESTAURANTS";
+	public RestaurantRepository() {
 
-	private Session session;
-
-	public RestaurantRepository(Session session) {
-		// TODO Auto-generated constructor stub
-		this.session = session;
 	}
 
 	public List<Restaurant> retrieveAll() {
@@ -32,10 +25,5 @@ public class RestaurantRepository {
 		return null;
 	}
 	
-	public void createTable() {
-        StringBuilder sb = new StringBuilder("CREATE TABLE IF NOT EXISTS ").append(TABLE_NAME).append("(").append("id uuid PRIMARY KEY, ").append("Name text,");
-        final String query = sb.toString();
-        session.execute(query);
-    }
 
 }
