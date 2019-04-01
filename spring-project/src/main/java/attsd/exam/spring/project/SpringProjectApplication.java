eaafd031ff4975f4c3d88f32b2b87e8122410a78
ChiaraRapicetta <chiara.rapicetta@stud.unifi.it>
2019-04-01 11:12:21 +0200

    Modified main class

--- spring-project/src/main/java/attsd/exam/spring/project/SpringProjectApplication.java
+++ spring-project/src/main/java/attsd/exam/spring/project/SpringProjectApplication.java
@@ -1,13 +1,25 @@
 package attsd.exam.spring.project;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.datastax.driver.core.Session;
+
+import attsd.exam.spring.project.repositories.KeyspaceRepository;
 
 @SpringBootApplication
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
+		
+		CassandraConnector connector = new CassandraConnector();
+		connector.connect("127.0.0.1", null);
+		Session session = connector.getSession();
+		
+		connector.close();
 		
 		
 	}
