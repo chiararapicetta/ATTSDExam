a51962ed7c6441a9fac4fb8f22dd7ff5361e7a41
ChiaraRapicetta <chiara.rapicetta@stud.unifi.it>
2019-04-02 18:11:34 +0200

    Added RestController test class and RestController implementation

---
+++ spring-project/src/main/java/attsd/exam/spring/project/controllers/RestaurantRestController.java
@@ -0,0 +1,26 @@
+package attsd.exam.spring.project.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import attsd.exam.spring.project.services.RestaurantService;
+
+@RestController
+public class RestaurantRestController {
+
+	@Autowired
+	private RestaurantService service;
+
+	@GetMapping("/api")
+	public String index() {
+		return "ok";
+
+	}
+
+	@GetMapping("/api/all")
+	public String getAll() {
+		return service.getAllRestaurants().toString();
+	}
+
+}
