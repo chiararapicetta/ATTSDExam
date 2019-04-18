838206ba6bd3437679cf7e1c9bde945ad6f97f32
giuliadallai <giulia.dallai93@gmail.com>
2019-04-18 16:42:43 +0200

    pom updated

--- spring-project/src/main/java/attsd/exam/spring/project/SpringProjectApplication.java
+++ spring-project/src/main/java/attsd/exam/spring/project/SpringProjectApplication.java
@@ -1,11 +1,7 @@
 package attsd.exam.spring.project;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
 
 
 
