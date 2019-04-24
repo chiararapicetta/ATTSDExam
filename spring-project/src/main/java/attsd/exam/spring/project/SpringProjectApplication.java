d4dd40c8ed397b73c49bf1591cbdd1e72646e676
giuliadallai <giulia.dallai93@gmail.com>
2019-04-24 21:12:15 +0200

    pom updated

--- spring-project/src/main/java/attsd/exam/spring/project/SpringProjectApplication.java
+++ spring-project/src/main/java/attsd/exam/spring/project/SpringProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class SpringProjectApplication {
 
 	
