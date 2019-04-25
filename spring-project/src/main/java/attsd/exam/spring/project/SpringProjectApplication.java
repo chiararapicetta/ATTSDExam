7ad2de51b6aa26a1e3960b195ce653010468e989
giuliadallai <giulia.dallai93@gmail.com>
2019-04-25 18:14:41 +0200

    springprojectapp updated

--- spring-project/src/main/java/attsd/exam/spring/project/SpringProjectApplication.java
+++ spring-project/src/main/java/attsd/exam/spring/project/SpringProjectApplication.java
@@ -2,12 +2,11 @@ package attsd.exam.spring.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+
+@SpringBootApplication
 public class SpringProjectApplication {
 
 	
