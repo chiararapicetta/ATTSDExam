5fd6dae87695efd083918ae17bedc95f65ba8982
giuliadallai <giulia.dallai93@gmail.com>
2019-04-27 09:44:50 +0200

    RestaurantWebControllerTest added

---
+++ spring-project/src/main/java/attsd/exam/spring/project/controllers/RestaurantWebController.java
@@ -0,0 +1,43 @@
+package attsd.exam.spring.project.controllers;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+import attsd.exam.spring.project.model.Restaurant;
+import attsd.exam.spring.project.services.RestaurantService;
+
+@Controller
+public class RestaurantWebController {
+
+	private RestaurantService restaurantService;
+
+	@Autowired
+	public RestaurantWebController(RestaurantService restaurantService) {
+		this.restaurantService = restaurantService;
+	}
+
+	@GetMapping("/")
+	public String index(Model model) {
+		List<Restaurant> allRestaurants = restaurantService.getAllRestaurants();
+		model.addAttribute("restaurants", allRestaurants);
+		model.addAttribute("message", allRestaurants.isEmpty() ? "No restaurant" : "");
+		return "index";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String editRestaurant(@PathVariable BigInteger id, Model model) {
+		Restaurant restaurantById = restaurantService.getRestaurantById(id);
+		model.addAttribute("restaurant", restaurantById);
+		model.addAttribute("message", restaurantById == null ? "No restaurant found with id: " + id : "");
+		return "edit";
+	}
+
+
+}
