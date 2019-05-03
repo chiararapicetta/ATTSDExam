a01dc95b43c60e025f55661bce8b9d5dda84e099
ChiaraRapicetta <chiara.rapicetta@stud.unifi.it>
2019-05-03 12:43:46 +0200

    added cucumber scenario

--- spring-project/src/bdd/java/attsd/exam/spring/project/controllers/steps/RestaurantWebControllerCucumberSteps.java
+++ spring-project/src/bdd/java/attsd/exam/spring/project/controllers/steps/RestaurantWebControllerCucumberSteps.java
@@ -2,6 +2,8 @@ package attsd.exam.spring.project.controllers.steps;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigInteger;
+
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -16,6 +18,7 @@ import org.springframework.test.context.ContextConfiguration;
 
 import attsd.exam.spring.project.controllers.webdriver.pages.AbstractPage;
 import attsd.exam.spring.project.controllers.webdriver.pages.HomePage;
+import attsd.exam.spring.project.model.Restaurant;
 import attsd.exam.spring.project.services.RestaurantService;
 import cucumber.api.PendingException;
 import cucumber.api.java.Before;
@@ -26,20 +29,20 @@ import cucumber.api.java.en.When;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(loader = SpringBootContextLoader.class)
 public class RestaurantWebControllerCucumberSteps {
-	
+
 	@Autowired
 	private RestaurantService restaurantService;
-	
+
 	@Autowired
 	private WebDriver webDriver;
-	
+
 	@LocalServerPort
 	private int port;
-	
+
 	private HomePage homePage;
-	
+
 	static final Logger LOGGER = Logger.getLogger(RestaurantWebControllerCucumberSteps.class);
-	
+
 	@TestConfiguration
 	static class WebDriverConfiguration {
 		@Bean
@@ -47,27 +50,37 @@ public class RestaurantWebControllerCucumberSteps {
 			return new HtmlUnitDriver();
 		}
 	}
-	
+
 	@Before
 	public void setup() {
 		AbstractPage.port = port;
 		LOGGER.info("Port set: " + port);
 	}
-	
+
 	@Given("^The database is empty$")
 	public void the_database_is_empty() throws Throwable {
-	    restaurantService.deleteAll();
+		restaurantService.deleteAll();
 	}
 
 	@When("^The User is on Home Page$")
 	public void the_User_is_on_Home_Page() throws Throwable {
-	    homePage = HomePage.to(webDriver);
+		homePage = HomePage.to(webDriver);
 	}
 
 	@Then("^A message \"([^\"]*)\" must be shown$")
 	public void a_message_must_be_shown(String expectedMessage) throws Throwable {
-	    assertThat(homePage.getBody()).contains(expectedMessage);
+		assertThat(homePage.getBody()).contains(expectedMessage);
 	}
 
-	
+	@Given("^Some restaurants are in the database$")
+	public void someRestaurantsAreInTheDatabase() throws Throwable {
+		restaurantService.storeInDb(new Restaurant(BigInteger.valueOf(1), "restaurant1", 10));
+		restaurantService.storeInDb(new Restaurant(BigInteger.valueOf(2), "restaurant2", 20));
+	}
+
+	@Then("^A table must show the restaurants$")
+	public void aTableMustShowTheRestaurants() throws Throwable {
+		assertThat(homePage.getRestaurantTableAsString()).isEqualTo("ID Name AveragePrice 1 restaurant1 10 2 restaurant2 20");
+	}
+
 }
--- spring-project/src/bdd/resources/homepage.feature
+++ spring-project/src/bdd/resources/homepage.feature
@@ -4,3 +4,8 @@ Scenario: List no restaurants
 	Given The database is empty
 	When The User is on Home Page
 	Then A message "No restaurants" must be shown
+	
+Scenario: List current restaurants
+	Given: Some restaurants are in the database
+	When: The User is on Home Page
+	Then: A table must show the restaurants
