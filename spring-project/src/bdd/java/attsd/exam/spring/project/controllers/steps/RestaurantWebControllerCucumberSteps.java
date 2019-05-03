2be33a7bb1dc88c7b03305e7e9ec53c77db23473
ChiaraRapicetta <chiara.rapicetta@stud.unifi.it>
2019-05-03 11:04:20 +0200

    update cucumber class

---
+++ spring-project/src/bdd/java/attsd/exam/spring/project/controllers/steps/RestaurantWebControllerCucumberSteps.java
@@ -0,0 +1,35 @@
+package attsd.exam.spring.project.controllers.steps;
+
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(loader = SpringBootContextLoader.class)
+public class RestaurantWebControllerCucumberSteps {
+	
+	@Given("^The database is empty$")
+	public void the_database_is_empty() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The User is on Home Page$")
+	public void the_User_is_on_Home_Page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^A message \"([^\"]*)\" must be shown$")
+	public void a_message_must_be_shown(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	
+}
