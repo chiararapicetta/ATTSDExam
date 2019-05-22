daa6d3bdf7017dbfa6448efa0b8584fdb68164fc
giuliadallai <giulia.dallai93@gmail.com>
2019-05-22 17:42:29 +0200

    RestaurantWebControllerIT created

--- spring-project/src/it/java/attsd/exam/spring/project/controllers/RestaurantWebControllerIT.java
+++ spring-project/src/it/java/attsd/exam/spring/project/controllers/RestaurantWebControllerIT.java
@@ -1,9 +1,7 @@
 package attsd.exam.spring.project.controllers;
 
 import static org.hamcrest.CoreMatchers.nullValue;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,9 +19,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.ModelAndViewAssert;
@@ -32,32 +31,41 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
+import attsd.exam.spring.project.config.WebSecurityConfig;
 import attsd.exam.spring.project.model.Restaurant;
-import attsd.exam.spring.project.services.RestaurantService;
+import attsd.exam.spring.project.repositories.RestaurantRepository;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = RestaurantWebController.class)
-public class RestaurantWebControllerTest {
+@Import({WebSecurityConfig.class})
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class RestaurantWebControllerIT {
 
-	private MockMvc mvc;
-	private MultiValueMap<String, String> params;
+
+	//@MockBean
+	//private RestaurantService restaurantService;
 	
 	@Autowired
 	private WebApplicationContext context;
 
-	@MockBean
-	private RestaurantService restaurantService;
+	@Autowired
+	private RestaurantRepository repository;
 
+	private MockMvc mvc;
+	
+	private MultiValueMap<String, String> params = new HttpHeaders();
+	
 	@Before
 	public void setup() {
 		mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+		repository.deleteAll();
 		params = new HttpHeaders();
 	}
 	
 	@After
 	public void clearAll() {
+		repository.deleteAll();
 		params.clear();
-}
+	}
 	
 	@Test
 	@WithMockUser
@@ -65,7 +73,6 @@ public class RestaurantWebControllerTest {
 		mvc.perform(get("/"))
 				.andExpect(view().name("index")).andExpect(status().isOk());
 	}
-
 	@Test
 	@WithMockUser
 	public void testStatus200() throws Exception {
@@ -84,27 +91,28 @@ public class RestaurantWebControllerTest {
 		mvc.perform(get("/")).andExpect(view().name("index"))
 				.andExpect(model().attribute("restaurants", new ArrayList<Restaurant>()))
 				.andExpect(model().attribute("message", "No restaurant"));
-		verify(restaurantService).getAllRestaurants();
+		assertEquals(0, repository.count());
 	}
+
 
 	@Test
 	@WithMockUser
-	public void testNotEmptyRestaurantList() throws Exception {
-		List<Restaurant> restaurants = Arrays.asList(new Restaurant(BigInteger.valueOf(1), "DaAndrea", 30));
-		when(restaurantService.getAllRestaurants()).thenReturn(restaurants);
+	public void testTwoRestaurants() throws Exception {
+		List<Restaurant> restaurants = Arrays.asList(new Restaurant(null, "Il Capriccio", 20), new Restaurant(null, "Seasons", 16));
+		repository.saveAll(restaurants);
 		mvc.perform(get("/")).andExpect(view().name("index")).andExpect(model().attribute("restaurants", restaurants))
 				.andExpect(model().attribute("message", ""));
-		verify(restaurantService).getAllRestaurants();
+		assertEquals(2, repository.count());
 	}
 
 	@Test
 	@WithMockUser
 	public void testSingleRestaurant() throws Exception {
 		Restaurant restaurant = new Restaurant(BigInteger.valueOf(1), "Zorba", 23);
-		when(restaurantService.getRestaurantById(BigInteger.valueOf(1))).thenReturn(restaurant);
+		repository.save(restaurant);
 		mvc.perform(get("/edit/1")).andExpect(view().name("edit"))
 				.andExpect(model().attribute("restaurant", restaurant)).andExpect(model().attribute("message", ""));
-		verify(restaurantService).getRestaurantById(BigInteger.valueOf(1));
+		assertEquals(1, repository.count());
 	}
 
 	@Test
@@ -113,7 +121,7 @@ public class RestaurantWebControllerTest {
 		mvc.perform(get("/edit/1")).andExpect(view().name("edit"))
 				.andExpect(model().attribute("restaurant", nullValue()))
 				.andExpect(model().attribute("message", "No restaurant found with id: 1"));
-		verify(restaurantService).getRestaurantById(BigInteger.valueOf(1));
+		assertEquals(0, repository.count());
 	}
 
 	@Test
@@ -125,7 +133,7 @@ public class RestaurantWebControllerTest {
 				.param("name", restaurant.getName())
 				.param("averagePrice","" + restaurant.getAveragePrice()))
 		.andExpect(view().name("redirect:/"));
-		verify(restaurantService).storeInDb(restaurant);
+		assertEquals(1, repository.count());
 	}
 
 	
@@ -135,20 +143,28 @@ public class RestaurantWebControllerTest {
 		mvc.perform(get("/new")).andExpect(view().name("edit"))
 				.andExpect(model().attribute("restaurant", new Restaurant(BigInteger.valueOf(1), null, 0)))
 				.andExpect(model().attribute("message", ""));
-		verify(restaurantService).getRestaurantById(BigInteger.valueOf(1));
 	}
 
 	@Test
 	@WithMockUser
 	public void testDeleteRestaurant() throws Exception {
+		Restaurant restaurant = new Restaurant(BigInteger.valueOf(1), "LaFiaccola", 45);
+		repository.save(restaurant);
+		Restaurant r = new Restaurant(BigInteger.valueOf(2), "CacioEPepe", 28);
+		repository.save(r);
 		mvc.perform(get("/delete/1")).andExpect(view().name("redirect:/"));
-		verify(restaurantService).delete(BigInteger.valueOf(1));
+		assertEquals(1, repository.count());
 	}
 	
 	@Test
 	@WithMockUser
 	public void testResetRestaurants() throws Exception {
+		Restaurant restaurant = new Restaurant(BigInteger.valueOf(1), "LaFiaccola", 45);
+		repository.save(restaurant);
+		Restaurant r = new Restaurant(BigInteger.valueOf(2), "CacioEPepe", 28);
+		repository.save(r);
 		mvc.perform(get("/reset")).andExpect(view().name("redirect:/"));
-		verify(restaurantService).deleteAll();
+		assertEquals(0, repository.count());
 	}
 }
+
