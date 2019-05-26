a42f6466280d6e844b410098c5a05ed962f9b2e2
ChiaraRapicetta <chiara.rapicetta@stud.unifi.it>
2019-05-26 19:58:37 +0200

    update class

--- spring-project/src/main/java/attsd/exam/spring/project/services/RestaurantService.java
+++ spring-project/src/main/java/attsd/exam/spring/project/services/RestaurantService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import attsd.exam.spring.project.model.Restaurant;
 import attsd.exam.spring.project.repositories.RestaurantRepository;
 
-@javax.annotation.ParametersAreNonnullByDefault
 @Service
 public class RestaurantService {
 
@@ -35,7 +34,6 @@ public class RestaurantService {
 		return restaurantRepository.save(r);
 	}
 
-	@javax.annotation.CheckForNull
 	public void delete(BigInteger id) {
 		Restaurant r = restaurantRepository.findById(id).orElse(null);
 		restaurantRepository.delete(r);
