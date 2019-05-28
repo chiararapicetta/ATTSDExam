016a424691d069b895d20a764788f67b952b8caf
giuliadallai <giulia.dallai93@gmail.com>
2019-05-28 16:07:44 +0200

    LoginControllerTest updated

--- spring-project/src/test/java/attsd/exam/spring/project/controllers/LoginControllerTest.java
+++ spring-project/src/test/java/attsd/exam/spring/project/controllers/LoginControllerTest.java
@@ -60,8 +60,8 @@ public class LoginControllerTest {
 	public void testNewUserWhenUserNotExists() throws Exception {
 		User user = new User();
 		mvc.perform(post("/signup")
-				.param("email", user.getEmail()).param("username", user.getUsername()).param("password", user.getPassword()))
-		.andExpect(model().attribute("successMessage", "User has been registered successfully"));
+				.param("email", user.getEmail()).param("username", user.getUsername()).param("password", user.getPassword())).andExpect(status().isOk());
+;
 	}
 	
 	@Test
